f5693212b16fa6f105705ddd4e40110b43e6b6ff
McLaren <artyudin@outlook.com>
2017-09-20 14:46:26 -0400

    Sep 20th

---
+++ scrape_pdf.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from io import StringIO
+from io import open
+import ssl
+
+
+def read_file(pdfFile):
+	rsrcmgr = PDFResourceManager()
+	retstr = StringIO()
+	laparams = LAParams()
+	device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+	process_pdf(rsrcmgr, device, pdfFile)
+	device.close()
+	content = retstr.getvalue()
+	retstr.close()
+	return content
