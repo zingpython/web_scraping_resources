fc837d46a543dd45a82272193bcebb76f6f85cc7
Art <artyudin@gmail.com>
2017-10-02 15:34:27 -0400

    Delete one.py

--- one.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def fetching_url():
-	html = requests.get("http://shakespeare.mit.edu/lll/full.html")
-	bsobj = BeautifulSoup(html.content, "html.parser")
-
-fetching_url()
