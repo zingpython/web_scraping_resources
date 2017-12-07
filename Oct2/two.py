c388e1aa834d7407f52cb77630b7b3e2de0d9ba0
McLaren <artyudin@outlook.com>
2017-12-07 17:42:43 -0500

    yes baby

--- Oct2/two.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def fetching_url():
-	html = requests.get("http://shakespeare.mit.edu/lll/full.html")
-	bsobj = BeautifulSoup(html.content, "html.parser")
-	print(bsobj)
-
-fetching_url()
